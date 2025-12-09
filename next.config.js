7bff1987545e7f7fe506d1c66e1f1f9d62a6bb8b
国家的男人 <guonan@guojiadenanrendeMacBook-Pro.local>
2025-12-09 19:33:08 +0800

    fix: vercel turbopack config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    return config;
-  },
-  turbopack: false,
+  // 必须保留 turbopack 配置为空对象，表示遵循默认设置
+  turbopack: {},
 };
 
 module.exports = nextConfig;
